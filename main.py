b620854b4e82c1b6ab6ee6f9c6121f631938de19
trentwiles <wilest7@gmail.com>
2023-10-04 10:50:14 -0700

    auth service start

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+import json
 
 app = Flask(__name__)
+app.secret_key = json.loads(open('config.json').read())["secret_key"]
+
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def welcome():
+    if 'username' not in session:
+        return render_template("index.html")
+    return render_template("dash.html")
 
 if __name__ == '__main__':
     app.run(port=5000)
